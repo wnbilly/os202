ed7cf575472fd5ae6377b907e096928eeff11c5e
wnbilly <weynansbilly@gmail.com>
2023-01-30 23:53:22 +0100

    last push for td1

--- td1/codes/ProdMatMat.cpp
+++ td1/codes/ProdMatMat.cpp
@@ -10,7 +10,7 @@
 namespace {
 void prodSubBlocks(int iRowBlkA, int iColBlkB, int iColBlkA, int szBlock,
                    const Matrix& A, const Matrix& B, Matrix& C) {
-  #pragma omp parallel for
+  //#pragma omp parallel for
   for (int j = iColBlkB; j < std::min(B.nbCols, iColBlkB + szBlock); j++)
     for (int k = iColBlkA; k < std::min(A.nbCols, iColBlkA + szBlock); k++)
       for (int i = iRowBlkA; i < std::min(A.nbRows, iRowBlkA + szBlock); ++i)
@@ -21,7 +21,7 @@ void prodSubBlocks(int iRowBlkA, int iColBlkB, int iColBlkA, int szBlock,
 
 Matrix operator*(const Matrix& A, const Matrix& B) {
   Matrix C(A.nbRows, B.nbCols, 0.0);
-  int szBlock = 512; //On admet que les matrices seront d'une taille supérieure à 32
+  int szBlock = 32; //On admet que les matrices seront d'une taille supérieure à 32
   //#pragma omp parallel for
   for (int iRowBlkA = 0; iRowBlkA < A.nbRows; iRowBlkA += szBlock)
     for (int iColBlkB = 0; iColBlkB < B.nbCols; iColBlkB += szBlock)
